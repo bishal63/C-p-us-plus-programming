9e2c0c1b4d7cdf67115b7a89c692ede4e7705804
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:02:23 +0600

    mahabub

---
+++ exercise_continue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    for(int a=1;a<=100;a=a+1)
+    {
+        if(a==10)
+        {
+        continue;
+        }
+        cout<<a<<endl;
+    }
+    getch;
+
+}
