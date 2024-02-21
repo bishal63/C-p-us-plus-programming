273ad913d9a98f7c258261d2fb5d27951973802d
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:55:30 +0600

    mahabub

---
+++ even_odd_problem_solving.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the a is=";
+    cin>>a;
+    (a%2==0)?cout<<a<<"number is even":cout<<a<<"number is odd";
+    getch;
+}
