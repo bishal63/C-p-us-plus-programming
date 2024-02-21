97ed57bd3f772da0676ec3e4271e801d0af9e1f1
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:59:53 +0600

    mahabub

---
+++ exercise_array_loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int array_number[]={10,20,30,40,50};
+    for(int i=0;i<5;i=i+1)
+    {
+        cout<<array_number[i]<<" ";
+    }
+    cout<<
+    getch;
+}
