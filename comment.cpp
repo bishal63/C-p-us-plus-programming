3612ac8403f6659c85039c7b6b15a0ce814d97a6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:44:27 +0600

    mahabub

---
+++ comment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    //user input nei program theke//
+    int a,b,sum;
+    cout<<"enter the two number-";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum is "<<sum;
+    getch();
+}
