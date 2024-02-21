5bc4990daef11cc888a1aae7c7b54186c12dc6f8
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:34:19 +0600

    mahabub

---
+++ string_chrachter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char mahabub []={'a','b','\0'};
+    char bishal [4]={'1','2','3'};
+    char alam []="mahabub alam bishal";
+    cout<<alam[1]<<endl;
+    cout<<bishal<<endl;
+    cout<<mahabub[1];
+    getch();
+}
