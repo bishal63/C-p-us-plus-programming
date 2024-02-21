f80553b142ee4417cf449ed664cddfc397bc7180
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:48:43 +0600

    mahabub

---
+++ constructor.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class student
+{
+    public:
+    int roll;
+    int registration;
+    void display()
+    {
+    cout<<roll<<endl<<registration<<endl;
+    }
+
+    student(int a, int b)
+    {
+        roll=a;
+        registration=b;
+        
+    }
+
+};
+int main()
+{
+    student mahabub(2344,565446);
+    student jibon(53635,74847);
+    mahabub.display();
+    jibon.display();
+
+    getch();
+}
