18852ad0331c75a03625da9d538abee2e2e2663a
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:40:20 +0600

    mahabub

---
+++ unary.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=+6;
+    cout<<a <<endl;
+    int b=-6;
+    cout<<b <<endl;
+    int c=6;
+    cout<<++c <<endl;   //increment
+    int d=6;
+    cout<<--d <<endl;          //decrement
+    int e=6;
+    cout<<e++ <<endl;        //increment
+    cout<<e <<endl;
+    int f=6;
+    cout<<f-- <<endl;       //decrement
+    cout<<f;
+
+
+    getch();
+}
