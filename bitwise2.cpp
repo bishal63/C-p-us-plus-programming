771fc0adb1fc109cae17a696f64884458f1b7cf5
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:41:07 +0600

    mahabub

---
+++ bitwise2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    a=32;
+    b=2;
+    c=a>>4;     //bitwise not
+    cout<<c <<endl;
+    c=b<<4;    //bitwise or
+    cout<<c;
+    getch();
+}
