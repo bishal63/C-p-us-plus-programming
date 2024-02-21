7584bd95841dd77ef0a52b40e7196be0f7c9f60e
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:38:47 +0600

    mahabub

---
+++ triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int base,height,square;
+
+    cout<<"the base is ";
+    cin>>base;
+
+    cout<<"the height is ";
+    cin>>height;
+
+    square=0.5*base*height;
+    cout<<"the area of triangle is "<<square;
+    getch();
+
+
+
+}
