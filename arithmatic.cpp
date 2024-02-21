4a299b2ca0e6e51c0fe017a7de483c61d2a5de36
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:39:09 +0600

    mahabub

---
+++ arithmatic.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    a=50;
+    b=50;
+
+    int sum=a+b;
+    cout<<"the sum is "<<sum;
+    cout<<endl;
+
+    int subtraction=a-b;
+    cout<<"the subtraction is "<<subtraction;
+    cout<<endl;
+
+    int mul=a*b;
+    cout<<"the multiplication is "<<mul;
+    cout<<endl;
+
+    int division=a/b;
+    cout<<"the division is "<<division;
+    cout<<endl;
+
+    int modulas=a%b;
+    cout<<"the modulas is "<<modulas;
+    getch();
+}
