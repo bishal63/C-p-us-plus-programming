76c88badc130cfa55f3d69d28d8acbb83e8d01c6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:27:12 +0600

    mahabub

---
+++ return_function.cpp
@@ -0,0 +1,28 @@
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+int mahabub(int a,int b)
+{
+    int sum=a+b;
+    return sum;
+}
+int main()
+{
+    cout<<mahabub(20,40);
+    
+    getch();
+}*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int bishal(int a,int b)
+{
+    int sum=a*b;
+    return sum;
+}
+int main()
+{
+    cout<<bishal(40,30);
+    getch();
+}
