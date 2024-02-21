59547bf6f30f396633eb3ebeea4b7c0898c6abdb
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:45:21 +0600

    mahabub

---
+++ conditional_input.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the any integer numbers:";
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"a is integer";
+    }
+    else
+    {
+        cout<<"a is not integer";
+    }
+    getch();
+}
