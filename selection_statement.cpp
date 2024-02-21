08e9076da297351ee1aa1fecefb223324632bb96
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:28:53 +0600

    mahabub

---
+++ selection_statement.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the value-";
+    cin>>a;
+    if(a<40)
+    {
+        cout<<"a is smaller then 100";
+    }
+    else if(a==50)
+    {
+        cout<<"a is smaller then 50";
+    }
+    else
+    {
+        cout<<"a is bigger then 100";
+    }
+    
+    getch();
+}
