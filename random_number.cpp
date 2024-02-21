b3c31c5097d2e92e439100279c828ca3a9773be6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:23:02 +0600

    mahabub

---
+++ random_number.cpp
@@ -0,0 +1,28 @@
+/*#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+using namespace std;
+void mahabub()
+{
+    int randomNumber=rand();
+    cout<<"first random number is: "<<randomNumber<<endl;
+}
+int main()
+{
+    mahabub();
+    getch();
+}*/
+
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    for(int i=0;i<=5;i=i+1)
+    {
+        int randomnumber=rand();
+        cout<<"random number are: "<<randomnumber<<endl;
+
+    }
+}
