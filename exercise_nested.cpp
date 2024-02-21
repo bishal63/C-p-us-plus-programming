10fc4c5cc8f3b88ce9cec9fecd16daaaa5f08f4b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:14:37 +0600

    mahabub

---
+++ exercise_nested.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter your input=";
+    cin>>a;
+    if(a>33)
+    {
+        if(a<=40)
+        {
+            cout<<"d";
+        }
+        else if(a<=50)
+        {
+            cout<<"c";
+        }
+        else if(a<=60)
+        {
+            cout<<"b";
+        }
+    }
+    else
+    {
+        cout<<"fail";
+    }
+    cout<<
+    getch;
+}
