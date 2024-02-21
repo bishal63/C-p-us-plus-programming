ee89a0f33efd9f3733d36ab1a0a4a68b1da6bd97
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:49:39 +0600

    mahabub

---
+++ continue_statement_loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    for(int a=1;a<=20;a=a+1)
+    {
+       if(a==10)
+       {
+            continue;
+       }
+       cout<<a<<"."<<"I love programming"<<endl;
+
+    }
+    cout<<"end of the loop";
+    getch();
+}
