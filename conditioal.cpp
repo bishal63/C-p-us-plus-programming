ecc1bfc94934c749c9f0a0ad0daee2a0863ea4b8
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:44:51 +0600

    mahabub

---
+++ conditioal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    a=10;
+    b=20;
+    if (a<b)
+    {
+        cout<<"true";
+    }
+    else
+    {
+        cout<<"false";
+    }
+    getch();
+}
