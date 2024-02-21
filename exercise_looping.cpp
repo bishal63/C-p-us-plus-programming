4e26e20d43a0cc59060f045b76f9fc050b5a5dae
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:12:59 +0600

    mahabub

---
+++ exercise_looping.cpp
@@ -0,0 +1,42 @@
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    for(int a=1;a<=100;a=a+1)
+    {
+    cout<<a<<"."<<"I love programming"<<endl;
+    }
+    getch;
+}*/
+
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=1;
+    while(a<=100)
+    {
+        cout<<a<<"."<<"I love programming"<<endl;
+        a=a+1;
+    }
+    
+    getch;
+}*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=1;
+    do
+    {
+        cout<<a<<"."<<"I love programming"<<endl;
+        a=a+1;
+    } 
+    while (a<=100);
+    getch;
+}
