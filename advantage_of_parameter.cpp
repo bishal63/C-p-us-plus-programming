d5496ef619a849bcc1cb5e293edce9463e87df1f
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:38:27 +0600

    mahabub

---
+++ advantage_of_parameter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void mahabub()
+{
+    int a=7*7;
+    cout<<"square number is: "<< a;
+}
+int main()
+{
+    mahabub();
+    getch();
+}
