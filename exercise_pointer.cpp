ed7b1685ef0e471b002202aea629740918d39671
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:18:26 +0600

    mahabub

---
+++ exercise_pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=20;
+    int *b;
+    b=&a;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<b<<endl;
+    cout<<*b;
+    getch;
+}
