652350be2629d631cb5dc56b44d5be4d8829b5c9
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:35:55 +0600

    mahabub

---
+++ string_input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"enter your name: ";
+    gets(name);
+    cout<<"wellcome "<< name;
+
+    getch();
+}
