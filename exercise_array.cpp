cf507fa7c7aef27aeeb02108102810735e37ecb6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:59:03 +0600

    mahabub

---
+++ exercise_array.cpp
@@ -0,0 +1,32 @@
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int array_number[5];
+    array_number[0]=10;
+    array_number[1]=20;
+    array_number[2]=30;
+    array_number[3]=40;
+    array_number[4]=50;
+    cout<<array_number[0]<<" ";
+    cout<<array_number[1]<<" ";
+    cout<<array_number[2]<<" ";
+    cout<<array_number[3]<<" ";
+    cout<<array_number[4]<<" ";
+    getch;
+}*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int array_number[]={10,20,30,40,50};
+    cout<<array_number[0]<<" ";
+    cout<<array_number[1]<<" ";
+    cout<<array_number[2]<<" ";
+    cout<<array_number[3]<<" ";
+    cout<<array_number[4]<<" ";
+    getch;
+}
