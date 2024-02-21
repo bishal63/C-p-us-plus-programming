acec5d7d1c80d933b0d5f5b332811d3b96371292
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:35:10 +0600

    mahabub

---
+++ string_exercise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"enter your input is"<<" ";
+    gets (name);
+    cout<<"my name is"<<" "<<name;
+    getch;
+}
