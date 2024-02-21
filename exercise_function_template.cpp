404eb6c2a78b1667dccb9b9baebe9f9cb9b8c855
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:11:41 +0600

    mahabub

---
+++ exercise_function_template.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+template<class bishal>
+bishal mahabub(bishal a,bishal b)
+{
+    return a+b;
+}
+int main()
+{
+    cout<<mahabub(20,30)<<endl;
+    cout<<mahabub(20.50,20.50);
+    getch;
+}
