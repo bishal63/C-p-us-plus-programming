3ff97939847f165cfa424aca4c55b631d7461b15
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:04:35 +0600

    mahabub

---
+++ exercise_formating1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<showpoint;
+    float a,b;
+    a=20.493457;
+    b=30.0;
+    float c=a+b;
+    cout<<c<<endl;
+
+    cout<<noshowpoint;
+    float e,f;
+    e=20.49876;
+    f=30.0;
+    float g=e+f;
+    cout<<g;
+
+    cout<<showpoint;
+    cout<<setprecision(10);
+    float x,y;
+    x=17.76534;
+    y=76.5776454;
+    float z=x+y;
+    cout<<z;
+    getch;
+}
