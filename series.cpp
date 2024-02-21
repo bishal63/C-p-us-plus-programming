5f0b0032bb4f7954f371ebb86d0e59ad6ea206f0
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 18:32:20 +0600

    mahabub

---
+++ series.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a=0,n;
+    cout<<"enter your N number is:";
+    cin>>n;
+    for(int i=1;i<=15;i=i+1)
+    {
+        a=a+i;
+    }
+    cout<<a;
+    getch;
+}
