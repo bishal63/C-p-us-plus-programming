9c4b64a705bc23dc6a03ed6a38f1cc6400d895fc
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 17:53:15 +0600

    mahabub

---
+++ default_parameter_function.cpp
@@ -0,0 +1,41 @@
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+void mahabub(int a=30,int b=50)
+{
+    int sum=a*b;
+    cout<<"sum is:"<<sum;
+}
+int main()
+{
+    mahabub();
+    getch();
+}*/
+
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+void mahabub(int a,int b=50)
+{
+    int sum=a*b;
+    cout<<"sum is:"<<sum;
+}
+int main()
+{
+    mahabub(30);
+    getch();
+}*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void mahabub(int a=30,int b=50)
+{
+    int sum=a*b;
+    cout<<"sum is:"<<sum;
+}
+int main()
+{
+    mahabub(10);
+    getch();
+}
